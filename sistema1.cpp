fe9b6683e584a3702696197b51b5d5699c0c8712
magalileal <magalileal@frba.utn.edu.ar>
2025-10-20 21:49:45 -0300

    modif mesa, es num, estado y ganancia?

--- sistema1.cpp
+++ sistema1.cpp
@@ -97,36 +97,36 @@ void bajaMesa() {
         cout << "Mesa no encontrada.\n";
 }
 
-void modifMesa(int codigoBuscado, int nuevaEdad) {
- FILE* archivo = fopen("estudiantes_binarios.dat", "rb+");
- if (archivo != NULL) {
- Estudiante estudiante;
- bool encontrado = false;
- while (!encontrado && fread(&estudiante, sizeof(Estudiante), 1, archivo) == 1) {
- if (estudiante.codigo == codigoBuscado) {
- encontrado = true;
- estudiante.edad = nuevaEdad;
- fseek(archivo, -sizeof(Estudiante), SEEK_CUR);
- fwrite(&estudiante, sizeof(Estudiante), 1, archivo);
- cout << "Edad actualizada exitosamente." << endl;
- }
- }
- if (!encontrado) {
- cout << "No se encontró un estudiante con ese código." << endl;
- }
- fclose(archivo);
- } else {
- cout << "No se pudo abrir el archivo para lectura y escritura." << endl;
- }
+void modifMesa(int numero_mesa, int nuevaEdad) {
+    FILE* archivo = fopen("mesas.dat", "rb+");
+    if (archivo != NULL) {
+    Mesa mesa;
+    bool encontrada = false;
+    while (!encontrada && fread(&mesa, sizeof(Mesa), 1, archivo) == 1) {
+    if (numero_mesa == mesaBuscada) {
+    encontrada = true;
+    numero_mesa = nuevoNum;
+    fseek(archivo, -sizeof(Mesa), SEEK_CUR);
+    fwrite(&mesa, sizeof(Mesa), 1, archivo);
+    cout << "Número de mesa actualizado." << endl;
+    }
+    }
+    if (!encontrada) {
+    cout << "No se encontró una mesa con ese número." << endl;
+    }
+    fclose(archivo);
+    } else {
+    cout << "No se pudo abrir el archivo para lectura y escritura." << endl;
+    }
 }
 int main() {
- int codigoBuscado, nuevaEdad;
- cout << "Ingrese el código del estudiante a buscar: ";
- cin >> codigoBuscado;
- cout << "Ingrese la nueva edad: ";
- cin >> nuevaEdad;
- editarEdadEstudiante(codigoBuscado, nuevaEdad);
- return 0;
+    int codigoBuscado, nuevaEdad;
+    cout << "Ingrese el código del estudiante a buscar: ";
+    cin >> codigoBuscado;
+    cout << "Ingrese la nueva edad: ";
+    cin >> nuevaEdad;
+    editarEdadEstudiante(codigoBuscado, nuevaEdad);
+    return 0;
 }
 
 
